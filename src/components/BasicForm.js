496bf90171be72b78cec85acc0ef310a9f728da2
Mr-Psmith <psancthus@gmail.com>
2023-06-08 17:48:06 +0000

    came back, finished it as I should

--- src/components/BasicForm.js
+++ src/components/BasicForm.js
@@ -1,126 +1,171 @@
-import { useState } from "react";
+//[import { useState } from "react";
 
-//import useInput from "../hooks/use-input";
+import useInput from "../hooks/use-input";
 
 const BasicForm = (props) => {
-  //const {enteredData, setEnteredData, enteredDataError, setEnteredDataError, dataHandler, clickedOutHandler, reset} = useInput();
-  const [enteredFirstName, setEnteredFirstName] = useState("");
-  const [enteredFirstNameError, setEnteredFirstNameError] = useState(false);
-  // const [firstNameTuched, setFirstNameTuched] = useState(false); //The "problem" is that I dont need the "tuchState", as my logic isnt inverted as max's is.....
-  // const enteredFirstNameValid = enteredFirstName.trim() !== ""; /* Bec it is reevaluating at every keystroke(onChange Handlers).... BUT if I use this I can leave out the enteredFirstNameError, but I will need the tuchState */
-              //For me my solution doesnt seems to be more difficult than Max's is 
+  const {
+    enteredData: enteredFirstName,
+    enteredDataError: enteredFirstNameError,
+    setEnteredDataError: setEnteredFirstNameError,
+    dataHandler: firstNameHandler,
+    clickedOutHandler: clickedOutFirstHandler,
+    reset: reset1,
+  } = useInput();
+  const {
+    enteredData: enteredLastName,
+    enteredDataError: enteredLastNameError,
+    setEnteredDataError: setEnteredLastNameError,
+    dataHandler: lastNameHandler,
+    clickedOutHandler: clickedOutSecondHandler,
+    reset: reset2,
+  } = useInput();
+  const {
+    enteredData: enteredEmail,
+    enteredDataError: enteredEmailError,
+    setEnteredDataError: setEnteredEmailError,
+    dataHandler: emailHandler,
+    clickedOutHandler: clickedOutEmailHandler,
+    reset: reset3,
+  } = useInput();
 
-  const [enteredLastName, setEnteredLastName] = useState("");
-  const [enteredLastNameError, setEnteredLastNameError] = useState(false);
-
-  const [enteredEmail, setEnteredEmail] = useState("");
-  const [enteredEmailError, setEnteredEmailError] = useState(false);
-
-  /* could have done this with useEffect as well but this way its more simple */
-  let formIsValid  = false;
-  if(enteredFirstName && enteredLastName && enteredEmail) {
+  let formIsValid = false;
+  if (enteredFirstName && enteredLastName && enteredEmail) {
     formIsValid = true;
   } else {
     formIsValid = false;
-  };
+  }
 
-  const firstNameHandler = event => {
-    setEnteredFirstName(event.target.value);
-    if (event.target.value.trim() !== "") { /* =>used enteredFirstName, but this is more correct, as this is always true the enteredFirstName might be incorrect if the state update is late. kinda. kinda we would be referring to an old state */
-      setEnteredFirstNameError(false);
-    }
-  };
-  const lastNameHandler = event => {
-    setEnteredLastName(event.target.value);
-    if (event.target.value.trim() !== "") {
-      setEnteredLastNameError(false);
-    }
-  };
-  const emailHandler = event => {
-    setEnteredEmail(event.target.value);
-    if (event.target.value.trim() !== "") { 
-      setEnteredEmailError(false);
-    }
-  };
-  
+  // const [enteredFirstName, setEnteredFirstName] = useState("");
+  // const [enteredFirstNameError, setEnteredFirstNameError] = useState(false);
 
-  const clickedOutFirstHandler = () => {
-    // setFirstNameTuched(true);
-    if (enteredFirstName.trim() === "") {
-      setEnteredFirstNameError(true);
-    }
-  };
-  const clickedOutSecondHandler = () => {
-    if (enteredLastName.trim() === "") {
-      setEnteredLastNameError(true);
-    }
-  };
-  const clickedOutEmailHandler = () => {
-    if (enteredEmail.trim() === "") {
-      setEnteredEmailError(true);
-    }
-  };
+  // const [enteredLastName, setEnteredLastName] = useState("");
+  // const [enteredLastNameError, setEnteredLastNameError] = useState(false);
 
-  const submitHandler = event => {
+  // const [enteredEmail, setEnteredEmail] = useState("");
+  // const [enteredEmailError, setEnteredEmailError] = useState(false);
+
+  // const [firstNameTuched, setFirstNameTuched] = useState(false); //The "problem" is that I dont need the "tuchState", as my logic isnt inverted as max's is.....
+  // const enteredFirstNameValid = enteredFirstName.trim() !== ""; /* Bec it is reevaluating at every keystroke(onChange Handlers).... BUT if I use this I can leave out the enteredFirstNameError, but I will need the tuchState */
+  //For me my solution doesnt seems to be more difficult than Max's is
+
+  /* could have done this with useEffect as well but this way its more simple */
+  // const firstNameHandler = event => {
+  //   setEnteredFirstName(event.target.value);
+  //   if (event.target.value.trim() !== "") { /* =>used enteredFirstName, but this is more correct, as this is always true the enteredFirstName might be incorrect if the state update is late. kinda. kinda we would be referring to an old state */
+  //     setEnteredFirstNameError(false);
+  //   }
+  // };
+  // const lastNameHandler = event => {
+  //   setEnteredLastName(event.target.value);
+  //   if (event.target.value.trim() !== "") {
+  //     setEnteredLastNameError(false);
+  //   }
+  // };
+  // const emailHandler = event => {
+  //   setEnteredEmail(event.target.value);
+  //   if (event.target.value.trim() !== "") {
+  //     setEnteredEmailError(false);
+  //   }
+  // };
+
+  // const clickedOutFirstHandler = () => {
+  //   // setFirstNameTuched(true);
+  //   if (enteredFirstName.trim() === "") {
+  //     setEnteredFirstNameError(true);
+  //   }
+  // };
+  // const clickedOutSecondHandler = () => {
+  //   if (enteredLastName.trim() === "") {
+  //     setEnteredLastNameError(true);
+  //   }
+  // };
+  // const clickedOutEmailHandler = () => {
+  //   if (enteredEmail.trim() === "") {
+  //     setEnteredEmailError(true);
+  //   }
+  // };
+
+  const submitHandler = (event) => {
     event.preventDefault();
     if (enteredFirstName.trim() === "") {
       setEnteredFirstNameError(true);
-    } 
+    }
     if (enteredLastName.trim() === "") {
       setEnteredLastNameError(true);
-    } 
+    }
     if (enteredEmail.trim() === "") {
       setEnteredEmailError(true);
     } else {
       console.log("Everything is okay!");
     }
-    if (enteredFirstName.trim() === "" || enteredLastName.trim() === "" || enteredEmail.trim() === "") {
+    if (
+      enteredFirstName.trim() === "" ||
+      enteredLastName.trim() === "" ||
+      enteredEmail.trim() === ""
+    ) {
       return;
     }
-    // if (enteredData.trim() === "") {
-    //   setEnteredDataError(true);
-    // }
-    // if (enteredData.trim() === "" ) {
-    //   return;
-    // }
-    console.log(enteredFirstName);
-    console.log(enteredLastName);
-    console.log(enteredEmail);
-    setEnteredFirstNameError(false);
-    setEnteredLastNameError(false);
-    setEnteredEmailError(false);
-    //reset();
+    console.log("Submitted!");
+    console.log(enteredFirstName, enteredLastName, enteredEmail);
+    // setEnteredFirstNameError(false);
+    // setEnteredLastNameError(false);
+    // setEnteredEmailError(false);
+    reset1();
+    reset2();
+    reset3();
   };
 
-  // const firstNameErr = !enteredFirstNameValid && firstNameTuched;
-
-  // const errorClass = enteredDataError ? "form-control invalid" : "form-control";
-
-  const errorClass1 = enteredFirstNameError ? "form-control invalid" : "form-control";
-  const errorClass2 = enteredLastNameError ? "form-control invalid" : "form-control";
-  const errorClass3 = enteredEmailError ? "form-control invalid" : "form-control";
-
+  const errorClass1 = enteredFirstNameError
+    ? "form-control invalid"
+    : "form-control";
+  const errorClass2 = enteredLastNameError
+    ? "form-control invalid"
+    : "form-control";
+  const errorClass3 = enteredEmailError
+    ? "form-control invalid"
+    : "form-control";
 
   return (
     <form onSubmit={submitHandler}>
-      <div className='control-group'>
+      <div className="control-group">
         <div className={errorClass1}>
-          <label htmlFor='name'>First Name</label>
-          <input type='text' id='name' onChange={firstNameHandler} onBlur={clickedOutFirstHandler} />
-          {enteredFirstNameError && <p className="error-text">Please enter a Valid first Name</p>}
+          <label htmlFor="name">First Name</label>
+          <input
+            type="text"
+            id="name"
+            onChange={firstNameHandler}
+            onBlur={clickedOutFirstHandler}
+          />
+          {enteredFirstNameError && (
+            <p className="error-text">Please enter a Valid first Name</p>
+          )}
         </div>
         <div className={errorClass2}>
-          <label htmlFor='name'>Last Name</label>
-          <input type='text' id='name' onChange={lastNameHandler} onBlur={clickedOutSecondHandler} />
-          {enteredLastNameError  && <p className="error-text">Please enter a Valid last Name</p>}
+          <label htmlFor="name">Last Name</label>
+          <input
+            type="text"
+            id="name"
+            onChange={lastNameHandler}
+            onBlur={clickedOutSecondHandler}
+          />
+          {enteredLastNameError && (
+            <p className="error-text">Please enter a Valid last Name</p>
+          )}
         </div>
       </div>
       <div className={errorClass3}>
-        <label htmlFor='name'>E-Mail Address</label>
-        <input type='text' id='name' onChange={emailHandler} onBlur={clickedOutEmailHandler} />
-        {enteredEmailError && <p className="error-text">Please enter a Valid Email</p>}
+        <label htmlFor="name">E-Mail Address</label>
+        <input
+          type="text"
+          id="name"
+          onChange={emailHandler}
+          onBlur={clickedOutEmailHandler}
+        />
+        {enteredEmailError && (
+          <p className="error-text">Please enter a Valid Email</p>
+        )}
       </div>
-      <div className='form-actions'>
+      <div className="form-actions">
         <button disabled={!formIsValid}>Submit</button>
       </div>
     </form>
--- src/hooks/use-input.js
+++ src/hooks/use-input.js
@@ -1,36 +1,41 @@
 //WELL AS I BUILT AN APP WITH LOGIC DIFFERENT FROM MAX I SIMPLY CANNOT WRAP MY HEAD AROUND MAX'S INVERT LOGIC, AND THIS WAY I CANNOT UNDERSTAND HOW THE F. SHOULD I DO THIS CUSTOM HOOK.
 //HOPEFULLY WE WILL BE EXERCISING MORE WITH CUST. HOOKS, IF NOT I WILL BE BACK
+//ONE ROUND LATER HE EXPLAINED THE THING HE SHOULD HAVE EXPLAINED EARLIER. SIMPLE AS "IF THERE IS 3 FORM INPUT, THAN YOU SHOULD DO THREE IMPORT OF INPUTS. LUL"
 
-// import { useState } from "react";
+import { useState } from "react";
 
-// const useInput = () => { /* for some reason we can expect a function as an argument */
+const useInput = () => {
 
-//   const [enteredData, setEnteredData] = useState("");
-//   const [enteredDataError, setEnteredDataError] = useState(false);
+  const [enteredData, setEnteredData] = useState("");
+  const [enteredDataError, setEnteredDataError] = useState(false);
 
-//   const dataHandler = event => {
-//     setEnteredData(event.target.value);
-//     if (event.target.value.trim() !== "") { 
-//     setEnteredDataError(false);
-//     }
-//   };
+  const dataHandler = event => {
+    setEnteredData(event.target.value);
+    if (event.target.value.trim() !== "") { 
+    setEnteredDataError(false);
+    }
+  };
 
-//   const clickedOutHandler = () => {
-//     if (enteredData.trim() === "") {
-//       setEnteredDataError(true);
-//     }
-//   };
+  const clickedOutHandler = () => {
+    if (enteredData.trim() === "") {
+      setEnteredDataError(true);
+    }
+  };
 
-//   const reset = () => {
-//     setEnteredDataError(false);
-//   };
+  const reset = () => {
+    setEnteredDataError(false);
+  };
 
-//   return {
-//     enteredData, setEnteredData, enteredDataError, setEnteredDataError, dataHandler, clickedOutHandler, reset
-//   };
+  return {
+    enteredData, setEnteredData, enteredDataError, setEnteredDataError, dataHandler, clickedOutHandler, reset
+  };
+
+};
+
+export default useInput;
 
 
-
+//const useInput = (someFunctionIDeletedSoDunnoTHeName) => { /* for some reason we can expect a function as an argument */
 //     const [enteredValue, setEnteredValue] = useState("");
 //     const [isTouched, setIsTouched] = useState(false);
 
@@ -53,6 +58,3 @@
 //     return {
 //         value: enteredValue, IsValid: valueIsValid, hasError/* = "hasError: hasError" */, valueChangeHandler, inputBlurHandler, reset
 //     };
-// };
-
-//export default useInput;
